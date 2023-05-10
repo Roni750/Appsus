a0711705347a8c15406392b9a3009f033fbbbd9c
roni750 <yerushalmironi@gmail.com>
2023-05-10 16:15:36 +0300

    Modified img's src

--- cmps/menu-modal.jsx
+++ cmps/menu-modal.jsx
@@ -5,10 +5,10 @@ export function MenuModal() {
     return (
             <div className="apps-menu">
                 <div className="apps-container">
-                <NavLink to="/"><img className="books-logo" src="../assets/img/books-logo.png" /><p>Books</p></NavLink>
-                <NavLink to="/about"><img className="about-logo" src="../assets/img/about-logo.png" /><p>About</p></NavLink>
-                <NavLink to="/mail"><img className="gmail-logo" src="../assets/img/gmail-logo.png" /><p>Mail</p></NavLink>
-                <NavLink to="/note"><img className="keep-logo" src="../assets/img/keep-logo.png" /><p>Keep</p></NavLink>
+                <NavLink to="/"><img className="books-logo" src="../../assets/img/books-logo.png" /><p>Books</p></NavLink>
+                <NavLink to="/about"><img className="about-logo" src="../../assets/img/about-logo.png" /><p>About</p></NavLink>
+                <NavLink to="/mail"><img className="gmail-logo" src="../../assets/img/gmail-logo.png" /><p>Mail</p></NavLink>
+                <NavLink to="/note"><img className="keep-logo" src="../../assets/img/keep-logo.png" /><p>Keep</p></NavLink>
                 </div>
             </div>
         
