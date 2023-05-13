e0237a065efff6eb6715d348106c147277576672
Ofek Rashti <ofekr2261@gmail.com>
2023-05-13 22:45:34 +0300

    img src fix

--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,25 @@
 export function Home() {
-    return (
-        <section className="home">
-            <div className="teaser-container">
-                <h2 className="">Welcome to Appsus!</h2>
-                <p>Introducing our revolutionary new app that seamlessly integrates email and notes to streamline your daily productivity. With our app, you can easily switch between writing notes and composing emails without missing a beat. Say goodbye to jumping between multiple apps and hello to a more productive you. Try it out today and see the difference for yourself.</p>
-                <h4 className="line-title">No need to sign up, you can start saving your notes & e-mailing right now!</h4>
-                <div className="img-container">
-                    <img className="note-teaser" src="/assets/img/notes-teaser.png" />
-                    <img className="teaser email" src="assets/img/email-teaser.png" />
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="home">
+      <div className="teaser-container">
+        <h2 className="">Welcome to Appsus!</h2>
+        <p>
+          Introducing our revolutionary new app that seamlessly integrates email
+          and notes to streamline your daily productivity. With our app, you can
+          easily switch between writing notes and composing emails without
+          missing a beat. Say goodbye to jumping between multiple apps and hello
+          to a more productive you. Try it out today and see the difference for
+          yourself.
+        </p>
+        <h4 className="line-title">
+          No need to sign up, you can start saving your notes & e-mailing right
+          now!
+        </h4>
+        <div className="img-container">
+          <img className="note-teaser" src="assets/img/notes-teaser.png" />
+          <img className="teaser email" src="assets/img/email-teaser.png" />
+        </div>
+      </div>
+    </section>
+  )
 }
